cd5b1eb9af44cae20ac1beda2419df801f7707bd
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-10-29 00:40:38 -0300

    feat: new responsive calendar

--- js/schedule.js
+++ js/schedule.js
@@ -119,7 +119,11 @@ function displayEventIfExist(dayString, dayElement) {
   if (event) {
     const eventDiv = document.createElement("div");
     eventDiv.classList.add("event");
-    eventDiv.innerText = event.title;
+
+    // Verifica o comprimento do título e aplica a truncagem
+    eventDiv.innerText =
+      event.title.length > 7 ? event.title.slice(0, 7) + "..." : event.title;
+
     dayElement.appendChild(eventDiv);
   }
 }
